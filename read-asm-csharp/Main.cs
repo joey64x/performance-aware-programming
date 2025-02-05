39a155a9b6ba6f9ff5374e502603aae2484692c0
Joey Lapegna <joseph.lapegna@scopely.com>
2025-02-04 16:58:41 -0800

    refactored part 1 homework into a single C# project

---
+++ read-asm-csharp/Main.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+class PerformanceAwareProgramming
+{
+    private static readonly Dictionary<string, (string Description, Action<string[]> Run)> Homework = new()
+    {
+        ["01"] = ("Single Instruction Decode", SingleInstructionDecode.Run),
+        //["02"] = ("Multi Instruction Decode", MultiInstructionDecode.Run)
+    };
+
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: dotnet run -- <homework_number> [homework_args]");
+            Console.WriteLine("Available homework:");
+            foreach (var hw in Homework)
+            {
+                Console.WriteLine($"  {hw.Key}: {hw.Value.Description}");
+            }
+            return;
+        }
+
+        if (Homework.TryGetValue(args[0], out var homework))
+        {
+            homework.Run(args[1..]);
+        }
+        else
+        {
+            Console.WriteLine($"Unknown homework number: {args[0]}");
+        }
+    }
+}
